98cf9edd99ba9799e20e95127a1c54f3e0d8bf39
SINSILS <edgaras389@gmail.com>
2023-03-23 20:37:00 +0200

    Minimal changes
    Didn't have many time this week, added:
    Collision intereaction between monster and student

---
+++ IT Students Journey/Assets/Scripts/Enemies/BlueEnemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BlueEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        Debug.Log("I was touched!!!");
+    }
+}
